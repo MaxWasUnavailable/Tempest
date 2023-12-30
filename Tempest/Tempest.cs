78b84d5a953346baa3be6db3ff75a739875b61f5
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-12-30 03:11:11 +0100

    Initial commit

---
+++ Tempest/Tempest.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Tempest;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Tempest : BaseUnityPlugin
+{
+    private new static readonly ManualLogSource Logger = new(PluginInfo.PLUGIN_NAME);
+    internal static Tempest Instance { get; private set; }
+    private static Harmony Harmony { get; set; }
+    private static bool IsPatched { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+
+        Harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+        PatchAll();
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    public void PatchAll()
+    {
+        if (IsPatched) return;
+        Harmony.PatchAll();
+        IsPatched = true;
+    }
+
+    public void UnpatchAll()
+    {
+        if (!IsPatched) return;
+        Harmony.UnpatchAll();
+        IsPatched = false;
+    }
+}
