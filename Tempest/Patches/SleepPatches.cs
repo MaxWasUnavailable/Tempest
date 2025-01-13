7d4ac1c4b34d40567bc15a0fc4e52680f8438fea
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2025-01-13 17:58:51 +0100

    Move to PlayerEvents

--- Tempest/Patches/SleepPatches.cs
+++ Tempest/Patches/SleepPatches.cs
@@ -14,69 +14,69 @@ internal static class SleepPatches
     [HarmonyPatch(nameof(Sleep.EnterBed))]
     private static bool SleepEnterBedPrefix(ref Transform bed)
     {
-        return SleepEvents.OnPreEnterBed(ref bed);
+        return PlayerEvents.OnPreEnterBed(ref bed);
     }
     
     [HarmonyPostfix]
     [HarmonyPatch(nameof(Sleep.EnterBed))]
     private static void SleepEnterBedPostfix(ref Transform bed)
     {
-        SleepEvents.OnPostEnterBed(ref bed);
+        PlayerEvents.OnPostEnterBed(ref bed);
     }
     
     [HarmonyPrefix]
     [HarmonyPatch(nameof(Sleep.LeaveBed))]
     private static bool SleepLeaveBedPrefix()
     {
-        return SleepEvents.OnPreLeaveBed();
+        return PlayerEvents.OnPreLeaveBed();
     }
     
     [HarmonyPostfix]
     [HarmonyPatch(nameof(Sleep.LeaveBed))]
     private static void SleepLeaveBedPostfix()
     {
-        SleepEvents.OnPostLeaveBed();
+        PlayerEvents.OnPostLeaveBed();
     }
     
     [HarmonyPrefix]
     [HarmonyPatch(nameof(Sleep.FallAsleep))]
     private static bool SleepFallAsleepPrefix()
     {
-        return SleepEvents.OnPreFallAsleep();
+        return PlayerEvents.OnPreFallAsleep();
     }
     
     [HarmonyPostfix]
     [HarmonyPatch(nameof(Sleep.FallAsleep))]
     private static void SleepFallAsleepPostfix()
     {
-        SleepEvents.OnPostFallAsleep();
+        PlayerEvents.OnPostFallAsleep();
     }
     
     [HarmonyPrefix]
     [HarmonyPatch(nameof(Sleep.WakeUp))]
     private static bool SleepWakeUpPrefix()
     {
-        return SleepEvents.OnPreWakeUp();
+        return PlayerEvents.OnPreWakeUp();
     }
     
     [HarmonyPostfix]
     [HarmonyPatch(nameof(Sleep.WakeUp))]
     private static void SleepWakeUpPostfix()
     {
-        SleepEvents.OnPostWakeUp();
+        PlayerEvents.OnPostWakeUp();
     }
     
     [HarmonyPrefix]
     [HarmonyPatch(nameof(Sleep.StartSleepTimeWarp))]
     private static bool SleepStartSleepTimeWarpPrefix()
     {
-        return SleepEvents.OnPreStartSleepTimeWarp();
+        return PlayerEvents.OnPreStartSleepTimeWarp();
     }
     
     [HarmonyPostfix]
     [HarmonyPatch(nameof(Sleep.StartSleepTimeWarp))]
     private static void SleepStartSleepTimeWarpPostfix()
     {
-        SleepEvents.OnPostStartSleepTimeWarp();
+        PlayerEvents.OnPostStartSleepTimeWarp();
     }
 }
