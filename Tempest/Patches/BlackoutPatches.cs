921c92e8f72977ff33d9a09a3d605bf0565f07c6
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2025-01-13 23:35:47 +0100

    Fix BlackoutPatches target

--- Tempest/Patches/BlackoutPatches.cs
+++ Tempest/Patches/BlackoutPatches.cs
@@ -9,14 +9,14 @@ namespace Tempest.Patches;
 internal static class BlackoutPatches
 {
     [HarmonyPrefix]
-    [HarmonyPatch(nameof(Blackout.FadeTo))]
+    [HarmonyPatch(nameof(Blackout.FadeTo), typeof(float), typeof(float))]
     private static bool BlackoutFadeToPrefix(ref float targetAlpha, ref float duration)
     {
         return !PlayerEvents.OnPreBlackout(ref targetAlpha, ref duration);
     }
     
     [HarmonyPostfix]
-    [HarmonyPatch(nameof(Blackout.FadeTo))]
+    [HarmonyPatch(nameof(Blackout.FadeTo), typeof(float), typeof(float))]
     private static void BlackoutFadeToPostfix(ref float targetAlpha, ref float duration)
     {
         PlayerEvents.OnPostBlackout(ref targetAlpha, ref duration);
